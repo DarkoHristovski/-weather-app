9c48c90f3ec5709598f9fec5e63d710139c183b1
Darko Hristovski <hristovski15@gmail.com>
2024-03-11 23:23:50 +0100

    adding feature for searching cities

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-
+import { useState } from 'react';
+import axios from 'axios';
 
 import './App.css'
+import Header from './components/Header/Header';
+import Search from './components/Search/Search';
+
+export type City = {
+  city:string;
+}
 
 function App() {
+  const [enteredCity, setEnteredCity] = useState<City>('');
 
+const cityHandler=(newCity:string)=>{
+setEnteredCity(newCity) }
+/*
   let apiKey= '627f2f8c91e540f7514a66b15560a3b3';
-  let url:string=`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`
+  let url:string=`http://api.openweathermap.org/geo/1.0/direct?q=London&appid=${apiKey}`
+
+  axios.get(url)
+  .then(res=>console.log(res.data))
+
+  */
+
+  {console.log(enteredCity)}
+
+
 
   return (
     <>
-    { console.log(url)}
+    <Header/>
+    <main>
+      <Search enterCity={cityHandler}/>
+     
+    </main>
     </>
   )
 }
